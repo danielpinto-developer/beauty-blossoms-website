596e54767844af5f87e9107dfef617c60021c15b
danielpinto-developer <danielpinto.developer@gmail.com>
2024-06-16 19:45:11 -0500

    testing5

---
+++ client.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC4eMq0Y8ERerdBIzsySqtG9QnisI3CBIc",
+    authDomain: "bb27studio-loyalty-program.firebaseapp.com",
+    projectId: "bb27studio-loyalty-program",
+    storageBucket: "bb27studio-loyalty-program.appspot.com",
+    messagingSenderId: "827670961717",
+    appId: "1:827670961717:web:9e7b9d33ddd047dfcc9b7c",
+    measurementId: "G-Y30PX1R10P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function displayClientInfo() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const phoneNumber = urlParams.get('phone');
+
+    try {
+        const userDoc = await getDoc(doc(db, "users", phoneNumber));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            const clientInfoDiv = document.getElementById('client-info');
+            clientInfoDiv.innerHTML = `<p>Name: ${userData.Name}</p>`;
+            if (userData.services && userData.services.length > 0) {
+                userData.services.forEach(service => {
+                    clientInfoDiv.innerHTML += `<p>Date: ${service.date} - Service: ${service.type}</p>`;
+                });
+            } else {
+                clientInfoDiv.innerHTML = '<p>No service records found.</p>';
+            }
+        } else {
+            document.getElementById('client-info').innerHTML = '<p>No records found.</p>';
+        }
+    } catch (error) {
+        console.error('Error displaying client info:', error);
+        document.getElementById('client-info').innerHTML = '<p>Error fetching records.</p>';
+    }
+}
+
+window.onload = displayClientInfo;
