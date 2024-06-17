6459ca1f79aaadaa2b5b1966b75d5d36200c82ab
danielpinto-developer <danielpinto.developer@gmail.com>
2024-06-16 22:51:13 -0500

    adding admin html and js

---
+++ admin.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC4eMq0Y8ERerdBIzsySqtG9QnisI3CBIc",
+    authDomain: "bb27studio-loyalty-program.firebaseapp.com",
+    projectId: "bb27studio-loyalty-program",
+    storageBucket: "bb27studio-loyalty-program.appspot.com",
+    messagingSenderId: "827670961717",
+    appId: "1:827670961717:web:9e7b9d33ddd047dfcc9b7c",
+    measurementId: "G-Y30PX1R10P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById('checkPhoneNumberButton').addEventListener('click', async () => {
+    const phoneNumber = document.getElementById('phone-number').value;
+    const messageElement = document.getElementById('message');
+    const dataContainer = document.getElementById('dataContainer');
+
+    if (!phoneNumber) {
+        alert('Please enter a phone number');
+        return;
+    }
+
+    try {
+        const userDoc = await getDoc(doc(db, "users", phoneNumber));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            messageElement.style.display = 'none';
+            window.location.href = `/admin.html?phone=${phoneNumber}&name=${encodeURIComponent(userData.Name)}`;
+        } else {
+            messageElement.style.display = 'block'; // Show message if account not found
+            messageElement.textContent = 'Cuenta no encontrada';
+            dataContainer.innerHTML = '';
+        }
+    } catch (e) {
+        console.error("Error fetching document: ", e);
+        messageElement.style.display = 'block';
+        messageElement.textContent = 'Error checking account';
+        dataContainer.innerHTML = '';
+    }
+});
