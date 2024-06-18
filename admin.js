e03117b006682e6967112e0369cdba33c070d396
danielpinto-developer <danielpinto.developer@gmail.com>
2024-06-18 14:29:56 -0500

    original working code6

--- admin.js
+++ admin.js
@@ -183,3 +183,11 @@ function groupServices(services) {
         return acc;
     }, {});
 }
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.style.display = 'none';
+    });
+    document.getElementById(tabId).style.display = 'block';
+}
