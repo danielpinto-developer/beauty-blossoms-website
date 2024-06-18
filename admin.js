544ae74db430015a5e7f7edaed82b5cfe1bcc22a
danielpinto-developer <danielpinto.developer@gmail.com>
2024-06-18 12:49:02 -0500

    fixing grid css3

--- admin.js
+++ admin.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore, doc, getDoc, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+    apiKey: "AIzaSyC4eMq0Y8ERerdBIzsySqtG9QnisI3CBIc",
+    authDomain: "bb27studio-loyalty-program.firebaseapp.com",
+    projectId: "bb27studio-loyalty-program",
+    storageBucket: "bb27studio-loyalty-program.appspot.com",
+    messagingSenderId: "827670961717",
+    appId: "1:827670961717:web:9e7b9d33ddd047dfcc9b7c",
+    measurementId: "G-Y30PX1R10P"
 };
 
 const app = initializeApp(firebaseConfig);
